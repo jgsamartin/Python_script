aa6527320bc5204da39d1dfc80a76ac0c3ee11c9
jdcdp <47483528+jdcdp@users.noreply.github.com>
2021-03-11 17:25:35 +0100

    Update Python_script.sh

--- Python_script.sh
+++ Python_script.sh
@@ -1,2 +1,2 @@
 sudo apt install python3-pip
-sudo pip3 install geopy geopandas pandas shapely tqdm shapely contextily
+sudo pip3 install numpy geopy geopandas pandas shapely tqdm shapely contextily
