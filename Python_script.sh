9cd3931fff5e5e26c3f8346b3c137a4b4e4f34c1
jdcdp <47483528+jdcdp@users.noreply.github.com>
2021-03-11 17:38:00 +0100

    Update Python_script.sh

--- Python_script.sh
+++ Python_script.sh
@@ -1,2 +1,3 @@
 sudo apt install python3-pip
-sudo pip3 install numpy geopy geopandas pandas tqdm shapely contextily matplotlib
+sudo -H pip3 install --upgrade pip
+sudo pip3 install numpy geopy geopandas pandas tqdm shapely contextily matplotlib skimage
